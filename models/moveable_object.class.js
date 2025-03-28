20bc519a380169cd06bcb6e259ddd9de52e66ab5
RobbyR98 <robbyrunge@yahoo.com>
2025-03-28 10:47:24 +0100

    add character animation

--- models/moveable_object.class.js
+++ models/moveable_object.class.js
@@ -4,10 +4,19 @@ class MoveableObject {
   img;
   height = 100;
   width = 100;
+  imageCache = {};
 
   loadImage(path) {
     this.img = new Image();
     this.img.src = path;
+  }
+
+  loadImages(array) {
+    array.forEach((path) => {
+      let img = new Image();
+      img.src = path;
+      this.imageCache[path] = img;
+    });
   }
 
   moveRight() {
