5896bf1db9e70742531290bfa1f56eac2603b0e3
RobbyR98 <robbyrunge@yahoo.com>
2025-03-31 06:09:10 +0200

    update addEventListener in game.js for keyboard control

--- js/game.js
+++ js/game.js
@@ -7,38 +7,38 @@ function init() {
   world = new World(canvas, keyboard);
 }
 
-window.addEventListener('keydown', (event =>{
-  if(event.keyCode == 37) {
+window.addEventListener('keydown', (event => {
+  if(event.code == 'ArrowLeft') {
     keyboard.LEFT = true;
   }
-  if(event.keyCode == 39) {
+  if(event.code == 'ArrowRight') {
     keyboard.RIGHT = true;
   }
-  if(event.keyCode == 38) {
+  if(event.code == 'ArrowUp') {
     keyboard.UP = true;
   }
-  if(event.keyCode == 40) {
+  if(event.code == 'ArrowDown') {
     keyboard.DOWN = true;
   }
-  if(event.keyCode == 32) {
+  if(event.code == 'Space') {
     keyboard.SPACE = true;
   }
 }));
 
-window.addEventListener('keyup', (event =>{
-  if(event.keyCode == 37) {
+window.addEventListener('keyup', (event => {
+  if(event.code == 'ArrowLeft') {
     keyboard.LEFT = false;
   }
-  if(event.keyCode == 39) {
+  if(event.code == 'ArrowRight') {
     keyboard.RIGHT = false;
   }
-  if(event.keyCode == 38) {
+  if(event.code == 'ArrowUp') {
     keyboard.UP = false;
   }
-  if(event.keyCode == 40) {
+  if(event.code == 'ArrowDown') {
     keyboard.DOWN = false;
   }
-  if(event.keyCode == 32) {
+  if(event.code == 'Space') {
     keyboard.SPACE = false;
   }
 }));
