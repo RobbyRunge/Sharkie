c5e8fc79bad151e3fea04646243c3651d1fad609
RobbyR98 <robbyrunge@yahoo.com>
2025-04-05 15:42:59 +0200

    add overlays for controls, imprint, and privacy policy with close button functionality

--- js/menu.js
+++ js/menu.js
@@ -3,4 +3,18 @@ function startGame() {
   startScreenRef.classList.add('d_none');
   // initLevel();
   console.log('start');
+};
+
+function openOverlay() {
+  let backgroundOverlayRef = document.getElementById('background_overlay');
+  backgroundOverlayRef.classList.add('background_overlay');
+  let closeButton = document.getElementById('close_overlay_btn');
+  closeButton.style.display = 'block';
+};
+
+function closeOverlay() {
+  let backgroundOverlayRef = document.getElementById('background_overlay');
+  backgroundOverlayRef.classList.remove('background_overlay');
+  let closeButton = document.getElementById('close_overlay_btn');
+  closeButton.style.display = 'none';
 }
